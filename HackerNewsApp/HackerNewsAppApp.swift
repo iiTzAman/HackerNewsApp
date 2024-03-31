720a999c680e7e47835919d5465e16855b2e93f0
iiTzAman <aman.giri2055@gmail.com>
2024-03-30 22:52:45 -0400

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Aman Giri on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
